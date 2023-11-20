661d2360e40a26e2d9cd073408a50df68fbc8150
Michael Owuor <michael.owuor@gmail.com>
2023-11-20 13:10:02 +0300

    feat: redeploy to github pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html'],
-	theme: {
-		fontFamily: {
-			ubuntu: 'Ubuntu, sans-serif',
-		},
-		extend: {
-			colors: {
-				redOrange: '#F63232',
-				orangeYellow: '#FEB614',
-				lightGray: '#F4F4F4',
-				darkGray: '#4B4B4B',
-				neon: '#71d300',
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/line-clamp'),
-		require('@tailwindcss/typography'),
-	],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      ubuntu: "Ubuntu, sans-serif",
+    },
+    extend: {
+      colors: {
+        redOrange: "#F63232",
+        orangeYellow: "#FEB614",
+        lightGray: "#F4F4F4",
+        darkGray: "#4B4B4B",
+        neon: "#71d300",
+      },
+    },
+  },
+  plugins: [],
 };
